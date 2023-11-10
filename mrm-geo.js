32b762a47f042fe29d0fef066e97dd18a703d4eb
Mike Maley <michael.r.maley@gmail.com>
2023-11-10 14:09:10 -0500

    added geo constraint

---
+++ mrm-geo.js
@@ -0,0 +1,141 @@
+// @ts-check
+
+/**
+ * @typedef Point2D
+ * @prop {number} x
+ * @prop {number} y
+ */
+
+/**
+ * @typedef GeoConstraint2D
+ * @prop {string} type constraint type
+ * @prop {Point2D[]} points points the constraint applies to
+ * @prop {Array<(function(): number)>} constraintEqs equation that is zero when  the constraint 
+ * is satisfied
+ * @prop {Array<Array<(function(): number)>>} constraintGrad deritative of the constraint
+ */
+
+/**
+ * Represents a 2D geometric system of points and constraints
+ */
+class GeometricSystem2D {
+
+  constructor(){
+    /** @type {Point2D[]} */
+    this.points = [];
+    /** @type {GeoConstraint2D[]} */
+    this.constraints = [];
+  }
+
+  /**
+   * @param {Point2D} point 
+   * @param {number} value 
+   * @returns {GeoConstraint2D}
+   */
+  static constFixedX(point, value) {
+    return {
+      type: "fixed x",
+      points: [point],
+      constraintEqs: [() => point.x - value],
+      constraintGrad: [[() => 1, () => 0]]
+    }
+  }
+
+  /**
+   * @param {Point2D} point 
+   * @param {number} value 
+   * @returns {GeoConstraint2D}
+   */
+  static constFixedY(point, value) {
+    return {
+      type: "fixed y",
+      points: [point],
+      constraintEqs: [() => point.y - value],
+      constraintGrad: [[() => 0, () => 1]]
+    }
+  }
+
+  /**
+   * @param {Point2D} point0
+   * @param {Point2D} point1
+   * @returns {GeoConstraint2D}
+   */
+  static constVert(point0, point1) {
+    return {
+      type: "vert",
+      points: [point0, point1],
+      constraintEqs: [() => point0.x - point1.x],
+      constraintGrad: [
+        [() =>  1, () => 0],
+        [() => -1, () => 0]
+      ]
+    }
+  }
+  
+  /**
+   * @param {Point2D} point0
+   * @param {Point2D} point1
+   * @returns {GeoConstraint2D}
+   */
+  static constHorz(point0, point1) {
+    return {
+      type: "horz",
+      points: [point0, point1],
+      constraintEqs: [() => point0.y - point1.y],
+      constraintGrad: [
+        [() => 0, () =>  1],
+        [() => 0, () => -1]
+      ]
+    }
+  }
+
+  /**
+   * @param {Point2D} p0
+   * @param {Point2D} p1
+   * @param {number} value distance between p0 and p1
+   * @returns {GeoConstraint2D}
+   */
+  static constDist(p0, p1, value) {
+    return {
+      type: "dist",
+      points: [p0, p1],
+      constraintEqs: [() => {
+        let dx = p1.x - p0.x;
+        let dy = p1.y - p0.y;
+        return dx*dx + dy*dy - value*value;
+      }],
+      constraintGrad: [
+        [() => p0.x - p1.x, () => p0.y - p1.y],
+        [() => p1.x - p0.x, () => p1.y - p0.y]
+      ]
+    }
+  }
+  
+  /**
+   * @param {Point2D} p0
+   * @param {Point2D} p1
+   * @param {Point2D} p2
+   * @returns {GeoConstraint2D}
+   */
+  static constColinear(p0, p1, p2) {
+    return {
+      type: "colinear",
+      points: [p0, p1, p2],
+      constraintEqs: [() => {
+        let ABx = p1.x - p0.x;
+        let ABy = p1.y - p0.y;
+        let ACx = p2.x - p0.x;
+        let ACy = p2.y - p0.y;
+        return ABx * ACy - ABy * ACx;
+      }],
+      constraintGrad: [
+        [() => p1.y - p2.y, () => p2.x - p1.x],
+        [() => p2.y - p0.y, () => p0.x - p2.x],
+        [() => p0.y - p1.y, () => p1.x - p0.x]
+      ]
+    }
+  }
+}
+
+
+
--- mrm-matrix.js
+++ mrm-matrix.js
@@ -9,15 +9,18 @@
  * @param {number[]} b 
  * @param {number} N number of interations
  */
-function GaussSeidel(A, x, b, N){
+function GaussSeidelMethod(A, x, b, N){
   const n = x.length;
-  console.assert(n == b.length, "size of x and b do not match");
+  if(n != b.length)
+    throw new Error("size of x and b do not match");
   for (let r=0; r<n; r++) {
-    console.assert(A[r].length == n, "size of A and x do not match");
-    console.assert(A[r][r] != 0, "diagonal element of A is 0");
+    if (A[r].length != n)
+      throw new Error("size of A and x do not match");
+    if (A[r][r] == 0)
+      throw new Error("diagonal element of A is 0");
   }
   for (let k=0; k<N; k++) { // iterations
-    for (let j=0; j<n; j++) { 
+    for (let j=0; j<n; j++) { // rows
       let sum = 0;
       for (let i=0; i<j; i++) {
         sum += A[j][i] * x[i];
@@ -31,14 +34,14 @@ function GaussSeidel(A, x, b, N){
   return x;
 }
 
-/* 
+
 test();
 function test(){
   let A = [ [ 16,  3],
             [ 7, -11]];
   let b = [11, 13];
   let x = [1, 1];
-  console.log(GaussSeidel(A, x, b, 6));
+  console.log(GaussSeidelMethod(A, x, b, 6));
   console.log(x);
   A = [ [10, -1,  2,  0],
         [-1, 11, -1,  3],
@@ -46,6 +49,12 @@ function test(){
         [ 0,  3, -1,  8]];
   b = [6, 25, -11, 15];
   x = [0, 0, 0, 0];
-  console.log(GaussSeidel(A, x, b, 4));
+  console.log(GaussSeidelMethod(A, x, b, 4));
+  A = [ [10, -1,  2,  0],
+        [-1,  0, -1,  3],
+        [ 2, -1, 10, -1],
+        [ 0,  3, -1,  8]];
+  b = [6, 25, -11, 15];
+  x = [0, 0, 0, 0];
+  console.log(GaussSeidelMethod(A, x, b, 4));
 }
- */
